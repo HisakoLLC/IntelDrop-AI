a3521598ecdba9b975566807c01b18564271646a
HisakoLLC <hisakolimited@gmail.com>
2026-03-28 20:53:16 +0300

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: "#000000",
+        white: "#FFFFFF",
+        // Enforce strict monochrome by overwriting default colors
+        gray: {
+          50: "#FFFFFF",
+          100: "#FFFFFF",
+          200: "#FFFFFF",
+          300: "#FFFFFF",
+          400: "#FFFFFF",
+          500: "#FFFFFF",
+          600: "#000000",
+          700: "#000000",
+          800: "#000000",
+          900: "#000000",
+          950: "#000000",
+        },
+      },
+      borderWidth: {
+        '3': '3px',
+      },
+    },
+  },
+  plugins: [],
+};
